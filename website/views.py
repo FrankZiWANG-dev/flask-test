0f63b08633d57acbf0fc32dca8ed8f3ec7d80e95
FrankZiWANG-dev <wang.frank.zi@gmail.com>
2021-12-06 15:08:49 +0100

    crud done

--- website/views.py
+++ website/views.py
@@ -24,27 +24,36 @@ def new_post():
 
     return render_template("todos.html", todos = todos)
 
-# @views.route("/edit-posts", methods=["GET", "POST"])
-# def edit_posts():
-#     if request.method == 'POST':
-#         title = request.form.get('title')
-#         url ='/edit-post?title='+title
+@views.route("/edit", methods=["GET", "POST"])
+def edit():
+    if request.method == 'POST':
+        id_edit = request.form.get('id-edit')
+        old_todo = Todo.query.filter_by(id=id_edit).first()
+        if old_todo:
+            db.session.delete(old_todo)
+            db.session.commit()
 
-#         return redirect(url)
+            title = request.form.get('title-edit')
+            content = request.form.get('content-edit')
+            
+            edited_todo = Todo(id=id_edit, title=title, content=content)
+            db.session.add(edited_todo)
+            db.session.commit()
+            return redirect(url_for('views.todos'))
 
-#     return render_template("edit-posts.html")
+        return redirect(url_for('views.todos'))
 
-# @views.route("/edit-post", methods=["GET", "POST"])
-# def edit_post():
-#     title = request.args.get('title')
-#     post = Post.query.filter_by(title="test")
-#     if request.method == 'POST':
-#         post.title = request.form.get('title')
-#         post.content = request.form.get('content')
-#         post.image = request.form.get('image')
-#         db.session.commit()
-#         redirect='/dashboard'
+    return render_template("todos.html", todos = todos)
 
-#         return redirect(url_for(redirect))
-    
-#     return render_template("edit-post.html", former_title=post.title, former_content=post.content, former_image=post.image)
+@views.route("/delete", methods=["GET", "POST"])
+def delete():
+    if request.method == 'POST':
+        id_delete = request.form.get('id-delete')
+        todo_delete = Todo.query.filter_by(id=id_delete).first()
+        if todo_delete:
+            db.session.delete(todo_delete)
+            db.session.commit()
+
+        return redirect(url_for('views.todos'))
+
+    return render_template("todos.html", todos = todos)
